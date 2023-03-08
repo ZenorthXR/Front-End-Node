9d16d9df4be51098dc687a73d687590033f9f13a
ZenorthXR <shinpawasri@gmail.com>
2023-03-08 10:04:49 +0700

    change structure

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
+//const base_url = "https://loacalhost:3000";
 const base_url = "http://node42138-shinnawat.proen.app.ruk-com.cloud:11279";
 
 // Set the template engine
