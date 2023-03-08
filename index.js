7bb0a6adc0d4d425eeb0bcdac170968310bd79e5
ZenorthXR <shinpawasri@gmail.com>
2023-03-08 10:32:01 +0700

    ud

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 // Base URL for the API
 //const base_url = "https://api.example.com";
 // const base_url = "http://localhost:3000";
-const base_url = "http://node42138-shinnawat.proen.app.ruk-com.cloud:11279";
+const base_url = "http://node42138-shinnawat.proen.app.ruk-com.cloud"
 
 
 
